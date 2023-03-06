284478d4f60ead6fe91601952f86ec06584c48b4
Edson Aguiar <edsondev.fortal@gmail.com>
2023-03-05 23:08:15 -0300

    :pencil: add new feature | go top button

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,49 @@
-// get DOM elements
+// ENABLE and DISABLE | dark mode
 const getSectionBtn = document.querySelector(".section-btn");
 const getTagMain = document.querySelector("main");
 
-// active dark mode
 getSectionBtn.addEventListener("click", () => {
   if (getTagMain.classList.contains("dark")) {
     getTagMain.classList.remove("dark");
   } else {
     getTagMain.classList.add("dark");
   }
+});
+
+// BUTTON | go to top site
+const goTopButton = document.getElementById("goTopButton");
+
+const setType_GoToButton = new TypeIt("#goTopButton", {
+  strings: "Up",
+  speed: 475,
+  startDelay: 200,
+  cursor: false,
+  loop: true,
+});
+
+function start_GoTopButton() {
+  const shouldShowButton =
+    window.scrollY > 650 && goTopButton !== undefined && goTopButton !== null;
+
+  if (shouldShowButton) {
+    setType_GoToButton.go();
+  } else {
+    // goTopButton.remove();
+    setType_GoToButton.reset();
+  }
+}
+
+function scrollFunction() {
+  const shouldShowButton = window.scrollY > 650;
+
+  if (shouldShowButton) {
+    goTopButton.classList.add("goTopButton");
+  } else {
+    goTopButton.classList.remove("goTopButton");
+  }
+}
+
+window.addEventListener("scroll", () => {
+  scrollFunction();
+  start_GoTopButton();
 });
