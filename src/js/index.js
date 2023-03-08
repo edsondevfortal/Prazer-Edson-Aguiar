e6657fecf53a1629b1dcf56291fe4a6bd164fb41
Edson Aguiar <edsondev.fortal@gmail.com>
2023-03-07 22:57:20 -0300

    :hammer: improve condicional and function-variable name

--- src/js/index.js
+++ src/js/index.js
@@ -10,10 +10,10 @@ getSectionBtn.addEventListener("click", () => {
   }
 });
 
-// BUTTON | go to top site
-const goTopButton = document.getElementById("goTopButton");
+// BUTTON | go to inicial page
+const goUpButton = document.getElementById("goTopButton");
 
-const setType_GoToButton = new TypeIt("#goTopButton", {
+const setType_goUpButton = new TypeIt("#goTopButton", {
   strings: "Up",
   speed: 475,
   startDelay: 200,
@@ -21,30 +21,29 @@ const setType_GoToButton = new TypeIt("#goTopButton", {
   loop: true,
 });
 
-function start_GoTopButton() {
-  const shouldShowButton =
-    window.scrollY > 650 && goTopButton !== undefined && goTopButton !== null;
+function scrollY_ToShowButton() {
+  const isValidScroll = window.scrollY > 650;
 
-  if (shouldShowButton) {
-    setType_GoToButton.go();
-  } else {
-    // goTopButton.remove();
-    setType_GoToButton.reset();
-  }
-}
-
-function scrollFunction() {
-  const shouldShowButton = window.scrollY > 650;
-
-  if (shouldShowButton) {
+  if (isValidScroll) {
     goTopButton.classList.add("goTopButton");
   } else {
     goTopButton.classList.remove("goTopButton");
   }
 }
 
+function start_GoUpButton() {
+  const isEnable = goTopButton.classList.contains("goTopButton");
+  const isValidScroll = window.scrollY > 650;
+
+  if (isEnable && isValidScroll) {
+    setType_goUpButton.go();
+  } else {
+    setType_goUpButton.reset();
+  }
+}
+
 window.addEventListener("scroll", () => {
-  scrollFunction();
+  scrollY_ToShowButton();
   start_GoTopButton();
 });
 
