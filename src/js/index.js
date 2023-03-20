7c74fdab35c0ff41b80432c6b117fc5d16736b11
Edson Aguiar <edsondev.fortal@gmail.com>
2023-03-20 02:06:15 -0300

    :hammer: improve js structure

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,5 @@
-// ENABLE and DISABLE | dark mode
-const getSectionBtn = document.querySelector(".section-btn");
-const getTagMain = document.querySelector("main");
-
-getSectionBtn.addEventListener("click", () => {
-  if (getTagMain.classList.contains("dark")) {
-    getTagMain.classList.remove("dark");
-  } else {
-    getTagMain.classList.add("dark");
-  }
-});
-
-// BUTTON | go to inicial page
-
-const goUpButton = document.getElementById("goUpButton");
-
-const setType_goUpButton = new TypeIt("#goUpButton", {
-  strings: "Up",
-  speed: 475,
-  startDelay: 200,
-  cursor: false,
-  loop: true,
-});
-
-function scrollY_ToShowButton() {
-  const isValidScroll = window.scrollY > 650;
-
-  if (isValidScroll) {
-    goUpButton.classList.add("goUpButton");
-  } else {
-    goUpButton.classList.remove("goUpButton");
-  }
-}
-
-function start_goUpButton() {
-  const goUpButtonn = document.getElementById("goUpButton");
-  const existButtonId = !!goUpButtonn; // !! convert to boolean value
-
-  const isValidScroll = window.scrollY > 650;
-
-  if (existButtonId && isValidScroll) {
-    setType_goUpButton.go();
-  } else {
-    setType_goUpButton.reset();
-  }
-}
-
-window.addEventListener("scroll", () => {
-  scrollY_ToShowButton();
-  start_goUpButton();
-});
-
-goUpButton.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    behavior: "smooth",
-  });
-});
-
 // TARGET SCROLL | smooth scroll to a target element
 const scrollToTarget = (targetElement) => {
-  // calcule a posição do elemento alvo na página
   const topOffset = targetElement.getBoundingClientRect().top + window.scrollY;
 
   window.scrollTo({
@@ -87,18 +27,22 @@ const hashLinks = document.querySelectorAll('a[href^="#"]');
 
 hashLinks.forEach(addLinkClickListener);
 
-const header = document.querySelector("header");
-console.log(header);
-
-window.addEventListener("scroll", function () {
-  const currentScrollPos = window.pageYOffset + 777;
-  const windowHeight = window.innerHeight;
-  const bodyHeight = document.body.clientHeight;
-  const bottomOffset = bodyHeight - windowHeight - currentScrollPos;
-  if (bottomOffset < header.clientHeight) {
-    header.classList.add("navbarPageTop");
-  } else {
-    header.classList.remove("navbarPageTop");
-  }
-  // this.alert(currentScrollPos);
-});
+// TypeIt | function that types custom texts
+new TypeIt("#my-name", {
+  speed: 100,
+  startDelay: 500,
+})
+  .type("Prazer, edison aguiar", { speed: 50, delay: 700 })
+  .move(-10, { delay: 700 })
+  .delete("i", { delay: 45 })
+  .move(-1, { delay: 800 })
+  .delete("e", { delay: 300 })
+  .type("É", { delay: 175 })
+  .delete("É", { delay: 400, instant: true })
+  .type("E", { delay: 175 })
+  .move(6, { delay: 200 })
+  .delete("a", { delay: 400, instant: true })
+  .type("A", { delay: 225 })
+  .move(5, { delay: 300 })
+  .type(" &#128513;")
+  .go();
